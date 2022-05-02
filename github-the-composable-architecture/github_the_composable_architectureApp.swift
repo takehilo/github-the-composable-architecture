006dbf5e66b8f28412ad6bc81d6471300b231c2d
takehilo <takehilo.kaneko@gmail.com>
2022-05-02 23:17:38 +0900

    Initial Commit

---
+++ github-the-composable-architecture/github_the_composable_architectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_the_composable_architectureApp.swift
+//  github-the-composable-architecture
+//
+//  Created by takehiro.kaneko on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct github_the_composable_architectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ github-the-composable-architectureUITests/github_the_composable_architectureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  github_the_composable_architectureUITestsLaunchTests.swift
+//  github-the-composable-architectureUITests
+//
+//  Created by takehiro.kaneko on 2022/05/02.
+//
+
+import XCTest
+
+class github_the_composable_architectureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
